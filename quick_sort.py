20c54418b2552ea5361c9929187849faf6f40c93
enderst3 <enderst3@gmail.com>
2017-01-21 13:45:15 -0800

    quick sort

---
+++ quick_sort.py
@@ -0,0 +1,18 @@
+'''
+write a function that sorts a list.
+'''
+
+
+def quicksort(start_list):
+    if len(start_list) < 1:
+        print(start_list)
+        return start_list
+
+    else:
+        print(sorted(start_list))
+        return sorted(start_list)
+
+quicksort([17, -5, 3])
+quicksort([3, 0, 7, 5, 1, 2, 9, 8, 4, 6])
+quicksort([])
+quicksort([9])
--- target_words.py
+++ target_words.py
@@ -1,8 +1,10 @@
 '''
-Write a function that finds the shortest possible text snippet in a given string, containing all of the target words
+Write a function that finds the shortest possible text snippet in a give
+string, containing all of the target words
 in an input list of strings.
 
-The function will take a string and a list as input, and returns a string as output, and return the earliest match.
+The function will take a string and a list as input, and returns a string as
+output, and return the earliest match.
 If there are no matches it returns none.
 
 In: 'many students are writing code', [students, code]
@@ -21,7 +23,6 @@ def target_words(input_string, input_list):
     for i in input_list:
         if i in input_string:
 
-
-    print(output_list)
+            print(output_list)
 
 target_words('a b c d a', ['a', 'c', 'd'])
