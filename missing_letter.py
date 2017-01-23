1e99fef304e2dd72d825026407b588914a2d3925
enderst3 <enderst3@gmail.com>
2017-01-23 11:20:18 -0800

    missing letters

---
+++ missing_letter.py
@@ -0,0 +1,26 @@
+'''
+Find the missing letter in the list
+'''
+import string
+
+
+def find_missing_letter(chars):
+    '''
+    charset = string.ascii_lowercase if chars[0] >= 'a' else string.ascii_uppercase
+    for letter in charset[charset.index(chars[0]):]:
+        if letter not in chars:
+            print(letter[0])
+            return letter[0]
+    '''
+
+    newlist = []
+
+    for i in range (ord(chars[0]), ((ord(chars[0])+len(chars) + 1))):
+        newlist.append(chr(i))
+
+    newitem = list(set(newlist) - set(chars))
+    print(', '.join(newitem))
+    return ''.join(newitem)
+
+find_missing_letter(['O','Q','R','S'])
+find_missing_letter(['a','b','d','f'])
