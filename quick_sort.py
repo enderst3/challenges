300c5d568587712638702c8833995b26b270c533
enderst3 <enderst3@gmail.com>
2017-01-29 21:56:52 -0800

    sort

--- quick_sort.py
+++ quick_sort.py
@@ -12,7 +12,7 @@ def quicksort(start_list):
         print(sorted(start_list))
         return sorted(start_list)
 
-quicksort([17, -5, 3])
+quicksort([17, -5, 3, -15])
 quicksort([3, 0, 7, 5, 1, 2, 9, 8, 4, 6])
 quicksort([])
 quicksort([9])
