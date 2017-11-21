2feb19140c04ae7aa5e005f734b368f1a9b9b746
enderst3 <enderst3@gmail.com>
2017-11-21 10:45:54 -0800

    router

--- react_apps/react_router_app/src/components/App.js
+++ react_apps/react_router_app/src/components/App.js
@@ -2,13 +2,19 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
+import Header from './Header';
+import Teachers from './Teachers';
+import Courses from './Courses';
 
 
 const App = () => (
   <BrowserRouter>
     <div className="container">
-      <Route path='/' component={Home} /> 
-      <Route path='/about' component={About} />
+      <Header />
+      <Route exact path='/' component={Home} /> 
+      <Route path='/about' render={ () => <About title='About' /> } />
+      <Route path='/teachers' component={Teachers} />
+      <Route path='/courses' component={Courses} />
     </div>
   </BrowserRouter>
 );
