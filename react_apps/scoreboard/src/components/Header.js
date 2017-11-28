e0c034dc37cce576c8325c2c458cdaf8d6688da9
enderst3 <enderst3@gmail.com>
2017-11-27 16:40:27 -0800

    redux components

--- react_apps/scoreboard/src/components/Header.js
+++ react_apps/scoreboard/src/components/Header.js
@@ -1,6 +1,18 @@
 import React, { Component, PropTypes } from 'react';
 
 export default class Header extends Component {
-
+  // function Header(props) {
+  //   return (
+  //     <div className="header">
+  //       <Stats players={props.players} />
+  //       <h1>Scoreboard</h1>
+  //       <Stopwatch />
+  //     </div>
+  //   );
+  // }
   
+  // Header.propTypes = {
+  //   players: React.PropTypes.array.isRequired,
+  // };
+
 }
--- react_apps/scoreboard/src/components/Player.js
+++ react_apps/scoreboard/src/components/Player.js
@@ -2,6 +2,25 @@ import React, { Component, PropTypes } from 'react';
 
 
 export default class Player extends Component {
+  function Player(props) {
+    return (
+      <div className="player">
+        <div className="player-name">
+          <a className="remove-player" onClick={props.onRemove}>✖</a>
+          {props.name}
+        </div>
+        <div className="player-score">
+          <Counter onChange={props.onScoreChange} score={props.score} />
+        </div>
+      </div>
+    );
+  }
   
+  Player.propTypes = {
+    name: React.PropTypes.string.isRequired,
+    score: React.PropTypes.number.isRequired,
+    onRemove: React.PropTypes.func.isRequired,
+    onScoreChange: React.PropTypes.func.isRequired,
+  };
     
 }
