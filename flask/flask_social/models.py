71baab4bd21aac98acec1d9eb62eb523889f00a7
enderst3 <enderst3@gmail.com>
2017-12-26 17:51:26 -0800

    flask social

---
+++ flask/flask_social/models.py
@@ -0,0 +1,31 @@
+import datetime
+
+from flask.ext.login import UserMixin
+from flask.ext.bcrypt import generate_password_hash, check_password_hash
+from peewee import *
+
+DATABASE = SqliteDatabase('social.db')
+
+class User(UserMixin, Model):
+    username = CharField(unique=True)
+    email = CharField(unique=True)
+    password = CharField(max_length=100)
+    joined_at = DateTimeField(default=datetime.datetime.now)
+    is_admin = BooleanField(default=False)
+
+    class Meta:
+        database = DATABASE
+        '''newest mebers first in the db.  the - will do that for us'''
+        order_by = ('-joined_at', )
+
+    '''cls will create the model instance when it runs the method.  Basically calls User.create'''
+    @classmethod
+    def create_user(cls, username, email, password, admin=False):
+        try:
+            cls.create(
+                username=username,
+                email=email,
+                password=generate_password_hash(password),
+                is_admin=admin)
+        except IntegrityError:
+            raise ValueError('User already exists')      
