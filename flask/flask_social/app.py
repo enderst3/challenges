3f09fb06c74cf2e0c010ca3f47847ad445dbaa98
enderst3 <enderst3@gmail.com>
2017-12-27 15:37:35 -0800

    flask social

---
+++ flask/flask_social/app.py
@@ -0,0 +1,48 @@
+from flask import Flask, g
+from flask.ext.login import LoginManager
+
+import models
+
+DEBUG = True
+PORT = 8000
+HOST ='0.0.0.0'
+
+app = Flask(__name__)
+app.secret_key = "l;kajdsofiupoasnkdf;nasdopywerwe;laksdj;fyopasydfasdf"
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manger.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(userid):
+    try:
+        return models.User.get(models.User.id == userid)
+    except models.DoesNotExist:
+        return None
+
+@app.before_request
+def before_request():
+    """Connect to the database before each request."""
+    g.db = models.DATABASE
+    g.db.connect()
+
+@app.after_request
+def after_request(response):
+    """Close the database connection after each request."""
+    g.db.close()
+    return response
+
+
+
+
+
+if __name___ == '__main__':
+    models.initialize()
+    models.User.create_user(
+        name='Johnny',
+        email='johnny@gmail.com',
+        password='password',
+        admin=True
+    )
+    app.run(debug=DEBUG, hose=HOST, port=PORT)
