98ff934a9ef676451f88d2a1e73bfef02eb86def
enderst3 <enderst3@gmail.com>
2018-02-25 10:21:04 -0800

    charts on redux

--- react_apps/redux-wf/src/containers/search_bar.js
+++ react_apps/redux-wf/src/containers/search_bar.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { fetchWeather } from '../actions/index'
 
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
   constructor(props) {
     super(props)
 
@@ -9,6 +12,7 @@ export default class SearchBar extends Component {
       term: ''
     }
     this.onInputChange = this.onInputChange.bind(this)
+    this.onFormSubmit = this.onFormSubmit.bind(this)
   }
 
   onInputChange(e) {
@@ -19,6 +23,11 @@ export default class SearchBar extends Component {
 
   onFormSubmit(e) {
     e.preventDefault()
+
+    this.props.fetchWeather(this.state.term)
+    this.setState({ 
+      term: '' 
+    })
   }
 
   render() {
@@ -45,3 +54,9 @@ export default class SearchBar extends Component {
     )
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch)
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar)
