d99c2ff2e760811a62285a5adc8af605905440d3
enderst3 <enderst3@gmail.com>
2018-05-30 22:15:50 -0700

    set_add"

---
+++ set_add.py
@@ -0,0 +1,40 @@
+"""
+Apply your knowledge of the .add() operation to help your friend Rupal.
+
+Rupal has a huge collection of country stamps. She decided to count the total number of distinct country stamps 
+in her collection. She asked for your help. You pick the stamps one by one from a stack of n country stamps.
+
+Find the total number of distinct country stamps.
+
+Input Format
+
+The first line contains an integer n, the total number of country stamps.
+The next n lines contains the name of the country where the stamp is from. 
+
+Output Format
+
+Output the total number of distinct country stamps on a single line.
+
+Sample Input
+
+7
+UK
+China
+USA
+France
+New Zealand
+UK
+France 
+Sample Output
+
+5
+Explanation
+
+UK and France repeat twice. Hence, the total number of distinct country stamps is 5 (five).
+"""
+
+num = input()
+dist = set()
+for i in range(int(num)):
+    dist.add(input())
+print(len(dist))
