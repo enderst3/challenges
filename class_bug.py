61f3acd691f9f02d29f5b6e6ce7d2201d6d72690
enderst3 <enderst3@gmail.com>
2018-09-10 19:40:17 -0700

    python challenge

---
+++ class_bug.py
@@ -0,0 +1,18 @@
+"""
+* Challenge   : The following code was thought to be working properly, however when the code tries to access the age of the person instance it fails.
+
+person = Person.new('Yukihiro', 'Matsumoto', 47)
+puts person.full_name
+puts person.age
+
+For this exercise you need to fix the code so that it works correctly.
+**********************************************************************
+"""
+
+class Person:
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.full_name = first_name + ' ' + last_name
