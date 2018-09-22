f97f914548db10ff30660af96fdb690b30728f75
enderst3 <enderst3@gmail.com>
2018-09-21 20:08:53 -0700

    sort

---
+++ sort_oe.py
@@ -0,0 +1,38 @@
+"""
+Given a string S. You have to return another string such that even-indexed and odd-indexed characters of S are grouped and groups are space-separated (see sample below)
+
+Note: 
+0 is considered to be an even index. 
+All input strings are valid with no spaces
+input: 'CodeWars' output 'CdWr oeas'
+S[0] = 'C'
+S[1] = 'o'
+S[2] = 'd'
+S[3] = 'e'
+S[4] = 'W'
+S[5] = 'a'
+S[6] = 'r'
+S[7] = 's'
+Even indices 0, 2, 4, 6, so we have 'CdWr' as the first group
+odd ones are 1, 3, 5, 7, so the second group is 'oeas'
+And the final string to return is 'Cdwr oeas'
+
+"""
+
+# enumerate
+def sort_my_string(S):
+    even = []
+    odd = []
+    for k,v in enumerate(S):
+        if k % 2 == 0:
+            even.append(v)
+        else:
+            odd.append(v)
+  
+    print(''.join(even)+" "+''.join(odd))
+    return ''.join(even)+" "+''.join(odd)
+
+
+# start stop step method
+def sort_my_string(s):
+    return s[::2] + ' ' + s[1::2]
