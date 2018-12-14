1a2e8d2b0da874256b793cd7b8ddbebe8f4d72b6
enderst3 <enderst3@gmail.com>
2018-12-14 12:34:13 -0800

    challenges

---
+++ odd_index.py
@@ -0,0 +1,10 @@
+"""
+You are given an array with several "even" words, one "odd" word, and some numbers mixed in.
+
+Determine if any of the numbers in the array is the index of the "odd" word. If so, return true, otherwise false.
+
+"""
+
+def odd_ball(arr):
+    def odd_ball(arr):
+    return arr.index('odd') in arr
