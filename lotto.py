575e0ac63dfc8e59edbf7f954d07fea6888ddcb4
enderst3 <enderst3@gmail.com>
2019-09-04 22:06:47 -0700

    lotto

--- lotto.py
+++ lotto.py
@@ -9,9 +9,9 @@ import random
 #Initialise an empty list that will be used to store the 6 lucky numbers!
 lotteryNumbers = []
 winning_numbers = []
-draws = 0
 
-for i in range (0,6):
+
+for i in range (0,5):
   number = random.randint(1,10)
   #Check if this number has already been picked and ...
   while number in lotteryNumbers:
@@ -24,24 +24,4 @@ for i in range (0,6):
 #Sort the list in ascending order
 lotteryNumbers.sort()
 print(lotteryNumbers)
-'''
-while winning_numbers.sort() != lotteryNumbers:
-    for i in range (0,2):
-        draw_number = random.randint(1,10)
-        #Check if this number has already been picked and ...
-        while draw_number in winning_numbers:
-            # ... if it has, pick a new number instead 
-            draw_number = random.randint(1,10)
-        
-        #Now that we have a unique number, let's append it to our list.
-        winning_numbers.append(draw_number)
-        draws += 1
-        print(draws)
-        
-winning_numbers.sort()
-print(winning_numbers)
-'''
-#Display the lsit on screen:
-# print(">>> Today's lottery numbers are: ") 
-# print(lotteryNumbers)
-# print("It took {} draws for you to win the lottery!".format(draws))
+
--- lotto_2.py
+++ lotto_2.py
@@ -2,17 +2,32 @@ import random
 
 winning_numbers = []
 ticket_numbers = []
-draw_numbers = []
-tickets = 0
 
+def ticket_draw():
+    # ticket numbers
+    for i in range(0,2):
+        number = random.randint(1,5)
+        while number in ticket_numbers:
+            number = random.randint(1,5)
+        ticket_numbers.append(number)
+    ticket_numbers.sort()
 
-
-def lotto_generator():
-    for i in range(0,4):
-        number = random.randint(1,10)
-        while number in draw_numbers:
-            number = random.randint(1,10)
-        draw_numbers.append(number)
-    print(draw_numbers.sort())
-
-lotto_generator()
+    # winning numbers
+    for i in range(0,2):
+        number = random.randint(1,5)
+        while number in winning_numbers:
+            number = random.randint(1,5)
+        winning_numbers.append(number)
+    winning_numbers.sort()
+    
+    # check to see if you won
+    if ticket_numbers == winning_numbers:
+        print("Your ticket {}".format(ticket_numbers))
+        print("Winning numbers {}".format(winning_numbers))
+        print('Congratulations!  You won!')
+    else:
+        print("Sorry you lose")
+        print("Your ticket {}".format(ticket_numbers))
+        print("Winning numbers {}".format(winning_numbers))
+          
+ticket_draw()
