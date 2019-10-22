8f2f054f9697548ea4afb4c216b92710ebf16eb9
enderst3 <enderst3@gmail.com>
2019-10-22 14:35:40 -0700

    21 advice

---
+++ 21_advice.py
@@ -0,0 +1,19 @@
+"""
+A function that advises a player on the best next move in a round of blackjack.
+For now, just use 15 as a Hit/Stay Threshold.
+>>> advise_player(10, 5)
+15 Hit.
+
+>>> advise_player('Q', 5)
+15 Hit.
+
+>>> advise_player('A', 'K')
+21 Blackjack!
+
+>>> advise_player('A', 'A')
+12 Hit.
+
+>>> advise_player('J', 'K')
+20 Stay.
+
+"""
