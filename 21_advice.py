1fc25ea4f2e3f518c8d5e99bcf7b55d066800d4b
enderst3 <enderst3@gmail.com>
2019-10-23 20:53:48 -0700

    21 advice

--- 21_advice.py
+++ 21_advice.py
@@ -17,3 +17,34 @@ For now, just use 15 as a Hit/Stay Threshold.
 20 Stay.
 
 """
+
+
+def advise_player(card_1, card_2):
+    face_cards = ['j', 'k', 'q']
+    
+    if str(card_1).lower() in face_cards:
+        card_1 = 10
+    elif str(card_1).lower() == 'a':
+        card_1 = 11
+    
+    if str(card_2).lower() in face_cards:
+        card_2 = 10
+    elif str(card_2).lower() == 'a':
+        if card_1 == 11:
+            card_2 = 1
+    
+    total = card_1 + card_2
+
+    if total < 16:
+        return '{} Hit.'.format(total)
+    elif total > 16 and total <= 20:
+        return '{} Stay.'.format(total)
+    elif total == 21:
+        return '{} Blackjack!'.format(total)
+    elif total > 21:
+        return '{} Bust!'.format(total)
+
+
+
+
+
