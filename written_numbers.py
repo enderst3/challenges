639db57ef982d56c6c051f756c5a384a004098b9
enderst3 <enderst3@gmail.com>
2019-10-26 21:28:21 -0700

    written numbers

--- written_numbers.py
+++ written_numbers.py
@@ -13,13 +13,17 @@ then prints out the name of it in English.
 
 """
 
-ones_teens = {1: 'One', 2: 'Two', 3: 'Three', 4: 'Four', 5: 'Five', \
-            6: 'Six', 7: 'Seven', 8: 'Eight', 9: 'Nine', 10: 'Ten', \
-            11: 'Eleven', 12: 'Twelve', 13: 'Thirteen', 14: 'Fourteen', \
-            15: 'Fifteen', 16: 'Sixteen', 17: 'Seventeen', 18: 'Eighteen', 19: 'Nineteen'}
-tens = ['Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety']
+ones_teens = {1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 
+            6: 'six', 7: 'seven', 8: 'eight', 9: 'nine', 10: 'ten', 
+            11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen', 15: 'fifteen',
+            16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 19: 'nineteen'}
+
+tens = ['twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
 
 def written(number):
     if number >= 1 and number < 19:
         # print(ones_teens[number])
         return ones_teens[number]
+    elif number >= 20 and number < 100:
+        first_digit, second_digit = divmod(number, 10)
+        return tens[first_digit - 2] + '-' + ones_teens[second_digit]
