d029fd64e7662dd834d63f234356525698fd86c2
enderst3 <enderst3@gmail.com>
2020-02-04 22:01:13 -0800

    more swap

--- pythonmc/swap_lists.py
+++ pythonmc/swap_lists.py
@@ -1,14 +1,16 @@
 '''
-def exchange_with(a, b):
-    a.reverse()
-    b.reverse()
-    a[:], b[:] = list(b), list(a)
+exchange the elements of two arrays in-place in a way that their new content is also reversed.
 '''
 
 a = ["1", "2", "3", "4", "5", "6", "7"]
 b = ["a", "b", "c"]
 
 def exchange_with(a, b):
+    a, b = b, a
     a.reverse()
     b.reverse()
-    a[:], b[:] = list(b), list(a)
+    print(a, b)
+   
+
+
+    
