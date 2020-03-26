0860afb48a2d655d013307dae24160075c0717a6
enderst3 <enderst3@gmail.com>
2020-03-25 22:21:32 -0700

    2d_lists

---
+++ 2d_lists.py
@@ -0,0 +1,14 @@
+# first method to create a 1D list
+n = 5 
+list = [0] * n
+print(list)
+
+print("="*20)
+
+# Second method to create a 1D list
+
+n = 5
+list = [0 for i in range(n)]
+print(list)
+
+print('='*20)
