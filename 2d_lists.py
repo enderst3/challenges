ed7304d640156ce1eb683486aa9b778ed3ea5e06
enderst3 <enderst3@gmail.com>
2020-04-14 22:08:25 -0700

    lists

--- 2d_lists.py
+++ 2d_lists.py
@@ -174,3 +174,21 @@ for row in a:
     print()
 
 print('='*20)
+
+'''
+Could also be list comprehension
+'''
+for row in a:
+    print(' '.join([str(elem) for elem in row]))
+
+print('='*20)
+
+'''
+You can use 2 nested loops to calculate the sum of all the numbers in the 2-dimensional list.
+'''
+a = [[1, 2, 3, 4], [5, 6], [7, 8, 9]]
+s = 0
+for i in range(len(a)):
+    for j in range(len(a[i])):
+        s += a[i][j]
+print(s)
